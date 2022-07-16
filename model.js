8b84f1494a340e200ee2cb72813c079612f9087d
subrat0796 <subratsahoo0796@gmail.com>
2022-07-16 16:45:25 +0530

    Some minor changes

--- model.js
+++ model.js
@@ -1,44 +1,45 @@
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 const DB = process.env.DATABASE;
 
+console.log(DB);
+
 //helps to get rid of Reference errors
-mongoose.connect(DB, {
-    useNewurlParser : true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-}).then(() =>{
+mongoose
+  .connect(DB)
+  .then(() => {
     console.log(`Connection Successful!`);
-}).catch((err) =>{
+  })
+  .catch((err) => {
+    console.log(err);
     console.log(`No connection!`);
-})
+  });
 
 let schema = new mongoose.Schema({
-    name : {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-        minlength: 6
-    },
-})
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 6,
+  },
+});
 
-schema.pre('save', (next) =>{
-    if(this.isModified('password')){
-        this.password = bcrypt.hash(this.password,10)
-    }
-    next();
-})
+schema.pre("save", (next) => {
+  if (this.isModified("password")) {
+    this.password = bcrypt.hash(this.password, 10);
+  }
+  next();
+});
 
-const Userdb = mongoose.model('Userdb',schema);
+const Userdb = mongoose.model("Userdb", schema);
 module.exports = Userdb;
--- server.js
+++ server.js
@@ -1,21 +1,22 @@
-const express = require('express');
-const dotenv = require('dotenv');
+const express = require("express");
+const dotenv = require("dotenv");
 // const token = require('jsonwebtoken');
 // const morgan = require('morgan');
 // const bodyparser = require('body-parser');
 // const path = require('path');
 
+dotenv.config({ path: "./config.env" });
+
 const app = express();
 
-const Userdb = require('./model');
+const Userdb = require("./model");
 app.use(express.json());
-app.use(require('./router'));
+app.use(require("./router"));
 
 // app.use(morgan('tiny'));
 
-dotenv.config({path: './config.env'})
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () =>{
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
